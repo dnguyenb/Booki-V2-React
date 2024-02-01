a2ace805d3223d3754769c2199d231c3c174cbc1
David Nguyen Blot <d.nguyenblot@gmail.com>
2024-02-01 18:42:21 +0100

    ajout router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
-import Header from '../components/Header';
+import Layout from '../pages/Layout';
+import Home from '../pages/Home';
+import NoPage from '../pages/NoPage';
 
-function App() {
-  return <Header />
-}
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />}/>
+					<Route path="*" element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+};
 
 export default App;
